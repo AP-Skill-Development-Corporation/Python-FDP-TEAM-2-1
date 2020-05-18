e652a6299b4db09455103cbfe246001ec5d8c925
Venkata Gopi <venkatagopi894@gmail.com>
2020-05-18 12:29:16 +0530

    Day1

---
+++ comments.py
@@ -0,0 +1,10 @@
+'''This is syntax of
+multiline
+comments'''
+# single line comment
+print('Hi i am from comments.py file')
+
+S = '''This is a mulitline
+string declaration
+in python'''
+print(S)
